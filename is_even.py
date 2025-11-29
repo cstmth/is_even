a14166065e69089a547c5613756997033c7f5fc1
Carl Dressler <ich@carldressler.de>
2025-11-29 13:13:50 +0100

    initial version

---
+++ is_even.py
@@ -0,0 +1,72 @@
+def is_even(n):
+    result = None
+
+    if n == 0:
+        result = True
+    if n == 1:
+        result = False
+    if n == 2:
+        result = True
+    if n == 3:
+        result = False
+    if n == 4:
+        result = True
+    if n == 5:
+        result = False
+    if n == 6:
+        result = True
+    if n == 7:
+        result = False
+    if n == 8:
+        result = True
+    if n == 9:
+        result = False
+    if n == 10:
+        result = True
+    if n == 11:
+        result = False
+    if n == 12:
+        result = True
+    if n == 13:
+        result = False
+    if n == 14:
+        result = True
+    if n == 15:
+        result = False
+    if n == 16:
+        result = True
+    if n == 17:
+        result = False
+    if n == 18:
+        result = True
+    if n == 19:
+        result = False
+    if n == 20:
+        result = True
+    if n == 21:
+        result = False
+    if n == 22:
+        result = True
+    if n == 23:
+        result = False
+    if n == 24:
+        result = True
+    if n == 25:
+        result = False
+    if n == 26:
+        result = True
+    if n == 27:
+        result = False
+    if n == 28:
+        result = True
+    if n == 29:
+        result = False
+    if n == 30:
+        result = True    
+
+    return result
+
+if __name__ == "__main__":
+    import sys
+    n = int(sys.argv[1])
+    print(is_even(n))
